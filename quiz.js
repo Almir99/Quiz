8c54e77bbe818af9029cbb0c94e51e023bff96e6
Almir99 <almir.osmanagic@live.com>
2022-07-26 13:57:05 +0200

    Fixed spelling errors

--- quiz.js
+++ quiz.js
@@ -1,30 +1,30 @@
-const Tacni_odgovori = ["A","B","A","A"];
+const Correct_answers = ["A","B","A","A"];
 const form = document.querySelector(".quiz-form");
-const Rezultat = document.querySelector(".result");
-const Procenat = document.querySelector('span');
+const Result = document.querySelector(".result");
+const Percentage = document.querySelector('span');
 
 form.addEventListener("submit", e =>{
     e.preventDefault();
     let score = 0;
 
-    const Dobijeni_odgovori = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-    Dobijeni_odgovori.forEach((vrijednost, index) =>{
-        if (vrijednost === Tacni_odgovori[index]){
+    const Answers_received = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    Answers_received.forEach((value, index) =>{
+        if (value === Correct_answers[index]){
             score++;
         }
     });
-    let vrijednost = 0;
-    let procenat = score/Tacni_odgovori.length*100;
-    console.log(procenat);
+    let value = 0;
+    let percentage = score/Correct_answers.length*100;
+    console.log(percentage);
     scrollTo(0,0);
     const timer = setInterval(() => {
-            Procenat.textContent = `${vrijednost}%`;
-            if (vrijednost === procenat){
+            Percentage.textContent = `${value}%`;
+            if (value === percentage){
                 clearInterval(timer);
             }
-            vrijednost++;
+            value++;
         }, 15
     )
 
-    Rezultat.classList.remove("d-none");
+    Result.classList.remove("d-none");
 })
